264fe05d9da06e56e5ff2b19a8fa45be91451c2c
Adam Yang <adamfyang@gmail.com>
2022-03-16 12:32:19 -0400

    ""

--- cropvalidation.py
+++ cropvalidation.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from pkgutil import get_data
 from typing_extensions import Self
 from idna import valid_contextj
@@ -10,12 +11,6 @@ from pyproj import Proj, transform
 import sklearn.metrics
 
 
-Stores and manipulates validation data.
-Stores and manipulates validation data.
-"""
-"""
-Stores and manipulates validation data.
-"""
 class ValidationSet():
     def __init__(self, path=None, projection=None):
         self.path = path
@@ -23,18 +18,20 @@ class ValidationSet():
         self.projection = None
 
         if projection != None:
-            self.projection = projection
-            self.validation = self.validation.set_crs(projection)
+            self.projection = Proj(projection)
+            self.validation = self.validation.set_crs(projection, allow_override=True)
 
         else:
-            self.projection = Proj('epsg:4326') #defaults to epsg 4326 if no projection is provided
-            self.validation = self.validation.set_crs(Proj('epsg:4326'))
+            self.projection = Proj('epsg:4326')
+            self.validation = self.validation.set_crs(Proj('epsg:4326'), allow_override=True)
 
     def reproject_validation(self, lat='lat', lon='lon', outProjection=None):
-        # Reprojects validation to a given epsg projection
-        newLat, newLon = reproject(self.validation[lat], self.validation[lon], self.getProjection(), outProjection)
+        newLat, newLon = reproject(self.validation[lat].values, self.validation[lon].values, self.projection, outProjection)
         self.validation[lat], self.validation[lon] = newLat, newLon
-        self.validation = self.validation.to_crs(outProjection)
+        #self.validation = self.validation.set_crs(outProjection, allow_override=True)
+
+    def filter(self):
+        self.validation = self.validation[(self.validation.crop_probability == 1) | (self.validation.crop_probability == 0)]
 
     def getProjection(self):
         return self.projection
@@ -51,31 +48,27 @@ class ValidationSet():
     def getLon(self):
         return self.validation['lon']
 
-"""
-CropMask wrapper, given as raster/tiff file. 
-"""
+
 class CropMask:
     def __init__(self, path, projection=None) -> None:
         self.path = path
         self.data = ras.open(path)
 
         if projection is None:
-            if self.projection is None:
-                self.projection = "epsg:4326"
+            if self.data.crs == None:
+                self.data.crs = Proj(4326)
             else:
-                self.projection = self.data.crs 
+                self.projection = self.data.crs
         else:  
             self.projection = Proj(projection)
 
-
-    def sample_points(self, lat, lon) -> np.array:
-        #samples points from a given set of coords and outputs coords and data points
+    def sample_points(self, lat, lon):
         if lat.size != lon.size:
             raise Exception("Input arrays are not of equal size.")
         coord_list = [(float(x),float(y)) for x,y in zip(lat, lon)]
         extracted_values = self.data.sample(coord_list)
 
-        return pd.DataFrame(zip(lat, lon, list(extracted_values)))
+        return pd.Series([x for x in extracted_values])
 
     def getProjection(self):
         return self.projection
@@ -89,26 +82,34 @@ class CropMask:
     def getName(self) -> str:
         return self.data.name
 
-"""
-Paired datastructure for validation and cropmask matches
-"""
+
 class MatchedSet():
     def __init__(self, cropmask, validation_data) -> None:
+        self.data = None
+        self.validation_data = None
+        self.cropmask = None
+
         if not (isinstance(cropmask, CropMask) and isinstance(validation_data, ValidationSet)):
             raise ValueError("Arguments not type CropMask or ValidationSet.")
         else:
             self.validation_data = validation_data
             self.cropmask = cropmask
             
-            self.data = pd.DataFrame(validation_data.getData()).astype(float)
-            self.data['cropmask'] = cropmask.sample_points(validation_data.getLat(), validation_data.getLon())[2].astype(float)
+            self.data = pd.DataFrame(self.validation_data.getData().reset_index(drop=True)).astype(float)
+            self.data['cropmask'] = (cropmask.sample_points(validation_data.validation.lat, validation_data.validation.lon)).astype(float)
         
     def remove_filler(self):
-        self.data = self.data.loc[self.data['crop_probability'].isin([0,1]) & self.data['cropmask'].isin([0,1])]
+        self.data = self.data[((self.data.crop_probability == 1) | (self.data.crop_probability == 0)) & ((self.data.cropmask == 1) | (self.data.cropmask == 0))]
+        #self.data = self.data[((self.data.crop_probability == 1) | (self.data.crop_probability == 0)) and (self.data.cropmask == 1) | (self.data.cropmask == 0))]
+
 
     def get_f1(self) -> float:
         #assumes dataset is already filtered 
         return sklearn.metrics.f1_score(self.data['crop_probability'], self.data['cropmask'])
+ 
+    def report(self) -> float:
+        #assumes dataset is already filtered 
+        return sklearn.metrics.classification_report(self.data['crop_probability'], self.data['cropmask'])
     
     def getData(self):
         return self.data
